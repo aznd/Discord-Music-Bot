1106eca721748b01d4439a33c4094375339efd82
aznd <80119822+aznd@users.noreply.github.com>
2022-07-27 20:40:16 +0200

    feat: implement slash commands

--- bot.py
+++ bot.py
@@ -1,35 +1,30 @@
+import discord
 from discord.ext import commands
+
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-client = commands.Bot(command_prefix='-')
+CLIENT_ID = os.getenv("CLIENT_ID")
+GUILD_ID = os.getenv("GUILD_ID")
+
+class MyBot(commands.Bot):
+    
+    def __init__(self):
+        super().__init__(
+                command_prefix='-',
+                intents = discord.Intents.default(),
+                application_id=CLIENT_ID
+        )
+
+    async def setup_hook(self):
+        await self.load_extension("cogs.music")
+        await bot.tree.sync()
 
 
-@client.command()
-async def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
-    await ctx.send("Loaded cog.")
+    async def on_ready(self):
+        print('Bot is ready.')
 
-
-@client.command()
-async def unload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-    await ctx.send("Unloaded cog.")
-
-
-@client.command()
-async def reload(ctx, extension):
-    try:
-        client.unload_extension(f'cogs.{extension}')
-        client.load_extension(f'cogs.{extension}')
-        await ctx.send("Reloaded cog.")
-    except Exception as e:
-        print(e)
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
-
-client.run(TOKEN)
+bot = MyBot()
+bot.run(TOKEN)
